92b9b3327eaafd25bc9241cea01a2fd1da324daf
GourmetsdeTarragona <anamonicaydami@gmail.com>
2025-04-23 13:30:43 +0200

    Create supabase.js

---
+++ src/lib/supabase.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://redojogbxdtqxqzxvyhp.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlZG9qb2dieGR0cXhxenh2eWhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3NjM1NTAsImV4cCI6MjA1OTMzOTU1MH0.HZMMSs3PWH1g2m00nr9Gczgz4DCIdU36nSvk_Sg8M68'; // tu clave anon completa
+export const supabase = createClient(supabaseUrl, supabaseKey);
