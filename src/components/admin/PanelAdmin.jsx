deb97eaa6ebf8ae28bcf60e5350251a6e335ab1a
GourmetsdeTarragona <anamonicaydami@gmail.com>
2025-04-25 12:59:37 +0200

    Update PanelAdmin.jsx

--- src/components/admin/PanelAdmin.jsx
+++ src/components/admin/PanelAdmin.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../../styles.css';
 
 const PanelAdmin = () => {
   const navigate = useNavigate();
@@ -8,12 +7,18 @@ const PanelAdmin = () => {
   return (
     <div className="page-container">
       <img src="/logo.png" alt="Logo" className="logo" />
-      <h2 className="page-title">Panel del Administrador</h2>
+      <h2 className="page-title">Panel de administración</h2>
 
       <div className="section">
-        <button onClick={() => navigate('/admin/nuevo-socio')}>Registrar nuevo socio/administrador</button>
-        <button onClick={() => navigate('/admin/nuevo-restaurante')}>Añadir nuevo restaurante</button>
-        <button onClick={() => navigate('/admin/categorias')}>Añadir categorías únicas (como vinos)</button>
+        <button onClick={() => navigate('/admin/nuevo-socio')}>
+          Añadir nuevo socio / administrador
+        </button>
+        <button onClick={() => navigate('/admin/nuevo-restaurante')}>
+          Añadir nuevo restaurante
+        </button>
+        <button onClick={() => navigate('/admin/categorias')}>
+          Añadir categorías especiales (vinos, etc.)
+        </button>
       </div>
     </div>
   );
