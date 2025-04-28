9cdda875c371ad1bc5530a8641eb5cf015702e9b
GourmetsdeTarragona <anamonicaydami@gmail.com>
2025-04-28 10:24:27 +0200

    Update LoginButton.jsx

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,15 +1,6 @@
-import { useNavigate } from 'react-router-dom';
-
-function LoginButton() {
-  const navigate = useNavigate();
-
-  const handleLogin = () => {
-    // Aquí después pondremos la lógica real de login
-    navigate('/restaurants'); // Simulamos que al iniciar sesión va a restaurantes
-  };
-
+function LoginButton({ onClick }) {
   return (
-    <button className="button-primary" onClick={handleLogin}>
+    <button className="button-primary" onClick={onClick}>
       Iniciar sesión
     </button>
   );
