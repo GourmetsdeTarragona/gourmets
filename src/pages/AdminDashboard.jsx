7387f798232ec7455a2c071de4f3bb60fd81c3ce
GourmetsdeTarragona <anamonicaydami@gmail.com>
2025-04-28 07:49:21 +0200

    Create AdminDashboard.jsx

---
+++ src/pages/AdminDashboard.jsx
@@ -0,0 +1,12 @@
+import AdminPanel from '../components/AdminPanel';
+
+function AdminDashboard() {
+  return (
+    <div className="container">
+      <h1>Panel de Administrador</h1>
+      <AdminPanel />
+    </div>
+  );
+}
+
+export default AdminDashboard;
