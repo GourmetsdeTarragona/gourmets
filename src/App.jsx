98fb2f7b0cd07f0a71c4386bbc425de77f1b6447
GourmetsdeTarragona <anamonicaydami@gmail.com>
2025-04-29 12:27:04 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Restaurants from './pages/Restaurants';
 import RestaurantVoting from './pages/RestaurantVoting';
 import Ranking from './pages/Ranking';
 import AdminDashboard from './pages/AdminDashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/restaurants" element={<Restaurants />} />
-        <Route path="/vote/:restaurantId" element={<RestaurantVoting />} />
         <Route path="/ranking" element={<Ranking />} />
-        <Route path="/admin" element={<AdminDashboard />} />
+        <Route
+          path="/restaurants"
+          element={
+            <ProtectedRoute allowedRoles={['socio']}>
+              <Restaurants />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/vote/:restaurantId"
+          element={
+            <ProtectedRoute allowedRoles={['socio']}>
+              <RestaurantVoting />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute allowedRoles={['admin']}>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
