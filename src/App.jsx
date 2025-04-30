c16cb8c6af87959baa8d6feab8e2d0977b2b9f44
GourmetsdeTarragona <anamonicaydami@gmail.com>
2025-04-30 22:13:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Restaurants from './pages/Restaurants';
-import RestaurantVoting from './pages/RestaurantVoting';
-import Ranking from './pages/Ranking';
-import AdminDashboard from './pages/AdminDashboard';
-import RegisterUser from './pages/RegisterUser';
-import CreateRestaurant from './pages/CreateRestaurant';
-import AdminRestaurants from './pages/AdminRestaurants';
-import AdminRestaurantDetail from './pages/AdminRestaurantDetail';
-import ProtectedRoute from './components/ProtectedRoute';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles.css';
 import { UserProvider } from './contexts/UserContext';
 
-function App() {
-  return (
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <UserProvider>
-      <Router>
-        <Routes>
-          {/* Acceso público */}
-          <Route path="/" element={<Home />} />
-          <Route path="/ranking" element={<Ranking />} />
-
-          {/* Zona socios */}
-          <Route
-            path="/restaurants"
-            element={
-              <ProtectedRoute allowedRoles={['socio']}>
-                <Restaurants />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/vote/:restaurantId"
-            element={
-              <ProtectedRoute allowedRoles={['socio']}>
-                <RestaurantVoting />
-              </ProtectedRoute>
-            }
-          />
-
-          {/* Zona administrador */}
-          <Route
-            path="/admin"
-            element={
-              <ProtectedRoute allowedRoles={['admin']}>
-                <AdminDashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/admin/register-user"
-            element={
-              <ProtectedRoute allowedRoles={['admin']}>
-                <RegisterUser />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/admin/create-restaurant"
-            element={
-              <ProtectedRoute allowedRoles={['admin']}>
-                <CreateRestaurant />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/admin/restaurantes"
-            element={
-              <ProtectedRoute allowedRoles={['admin']}>
-                <AdminRestaurants />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/admin/restaurante/:id"
-            element={
-              <ProtectedRoute allowedRoles={['admin']}>
-                <AdminRestaurantDetail />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </Router>
+      <App />
     </UserProvider>
-  );
-}
+  </React.StrictMode>
+);
 
-export default App;
