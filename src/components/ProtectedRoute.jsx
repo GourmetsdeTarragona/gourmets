e0f94e1ca4eb1bb37f6dc86629ae8ea15d6c2081
GourmetsdeTarragona <anamonicaydami@gmail.com>
2025-04-30 23:41:06 +0200

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,19 +1,19 @@
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext.jsx';
-import AccessDenied from '../pages/AccessDenied';
+import { useUser } from '../contexts/UserContext';
 
 function ProtectedRoute({ children, allowedRoles }) {
-  const { user } = useAuth();
+  const { user } = useUser();
 
   if (!user) {
-    return <Navigate to="/" />;
+    return <p>Cargando...</p>;
   }
 
-  if (allowedRoles && !allowedRoles.includes(user.rol)) {
-    return <AccessDenied />;
+  if (!allowedRoles.includes(user.rol)) {
+    return <Navigate to="/access-denied" replace />;
   }
 
   return children;
 }
 
 export default ProtectedRoute;
+
