36741c418e7aad3ed07f9c3a3c20c8ff35e2476f
GourmetsdeTarragona <anamonicaydami@gmail.com>
2025-06-06 07:11:52 +0200

    Update supabase.js

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,13 +1,7 @@
-
+// src/lib/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://redojogbxdtqxqzxvyhp.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlZG9qb2dieGR0cXhxenh2eWhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3NjM1NTAsImV4cCI6MjA1OTMzOTU1MH0.HZMMSs3PWH1g2m00nr9Gczgz4DCIdU36nSvk_Sg8M68';
 
-export const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: true,
-  },
-});
+export const supabase = createClient(supabaseUrl, supabaseKey);
